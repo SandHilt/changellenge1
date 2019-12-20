e9358d8f752e445634dfeb0356eb147c2b72c85b
Bruno Ochotorena <hilt@outlook.com.br>
2019-12-20 00:59:13 -0300

    Adicionando funcionalidade de pegar os dados via JSON

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 // $(function() {});
 
-$('#create-event').on('click', function() {
-	alert('criando evento');
+$('#create-event').on('click', function () {
+    alert('criando evento');
+});
+
+$.getJSON('./data.json', function (data) {
+    $.each(data, function (_, value) {
+        var item = [], activeClass;
+
+        ["Nome", "Data", "Local"].forEach(function (field) {
+            item.push("<td>" + value[field] + "</td>");
+        });
+
+        if (value["Ativo"] === "Sim") {
+            activeClass = "success";
+        } else {
+            activeClass = "danger"
+        }
+
+        item.push("<td class='text-" + activeClass + "'>" + value["Ativo"] + "</td>");
+
+        // ["Ver", "Editar", "Excluir"];
+
+        $("<tr/>", {html: item.join("")}).appendTo("#data");
+    });
 });
