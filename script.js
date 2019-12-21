f36ae1735bf2e98c63dfcb6d5ef780a31d93df7a
Bruno Ochotorena <hilt@outlook.com.br>
2019-12-21 16:45:21 -0300

    Mais melhorias no código

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// $(function() {});
-
 $('#create-event').on('click', function () {
     alert('criando evento');
 });
 
 $.getJSON('./data.json', function (data) {
     $.each(data, function (_, value) {
-        var item = [], activeClass;
+        var item = [];
+        var activeClass;
+        var btnGroup = [];
 
         ["Nome", "Data", "Local"].forEach(function (field) {
-            item.push("<td>" + value[field] + "</td>");
+            item.push("<td class='text-muted'>" + value[field] + "</td>");
         });
 
         if (value["Ativo"] === "Sim") {
@@ -20,7 +20,20 @@ $.getJSON('./data.json', function (data) {
 
         item.push("<td class='text-" + activeClass + "'>" + value["Ativo"] + "</td>");
 
-        // ["Ver", "Editar", "Excluir"];
+        ["eye", "edit", "trash-alt"].forEach(function (value) {
+            // btnGroup.push("<button type='button' class='btn-primary mr-1'>" + value + "</button>");
+            btnGroup.push(
+                "<button type='button' class='btn'>" +
+                "<i class='text-primary fa-lg far fa-" + value + "'></i>" +
+                "</button>"
+            );
+        });
+
+        item.push("<td>" +
+            "<span class='btn-group btn-group-sm' role='group' aria-label='Ações com a linha'>" +
+            btnGroup.join(' ') + "</span>" +
+            "</td>"
+        );
 
         $("<tr/>", {html: item.join("")}).appendTo("#data");
     });
